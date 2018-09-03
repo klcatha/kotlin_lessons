038066e7f26fe61f6a39320aac0c2a9bdd04cb20
klcatha <klcatha0518@gmail.com>
2018-09-04 08:45:07 +0900

    add comment

--- 08_if/myapp.kt
+++ 08_if/myapp.kt
@@ -9,6 +9,6 @@ fun main(args: Array<String>) {
   }else{
     println("so so ...")
   }*/
-  val result = if (score > 80) "Great" else "so so ..."
+  val result = if (score > 80) "Great" else "so so ..." // 式として表現
   println(result)
 }
