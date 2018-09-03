6e8ac2b5acf6d359c5d749ae2acbb4217783262a
klcatha <klcatha0518@gmail.com>
2018-09-04 07:19:52 +0900

    add lesson 07

---
+++ 07_string/myapp.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+  // 文字列
+  println("hello " + "world")
+
+  val name = "tacuchi"
+  println("my name is $name")
+
+  println("my score is ${12 + 32}")
+
+  // \n: 改行
+  // \t: タブ
+  println("hello\n wor\tld")
+  
+}
