d677e67cc38cc4cbbcafb0c0604e11a3830e82c2
klcatha <klcatha0518@gmail.com>
2018-09-04 06:45:50 +0900

    add lesson 06

---
+++ 06_calc/myapp.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>){
+  // + - * / %
+  val x = 10
+  println(x / 3) // 3
+  println(x / 3.0) // 3.33333...
+  println(x % 3) // 1
+
+  // ++ --
+  var y = 5
+  y++
+  println(y) // 6
+  y--
+  println(y) // 5
+
+  var z = 4
+  /* z = z + 12 */
+  z *= 12
+
+  // AND OR NOT
+  // && || !
+  val flag = true
+  println(!flag) // false
+}
