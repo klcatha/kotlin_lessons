9b538ae78212bb876e1ef2f191ad94c823331531
klcatha <klcatha0518@gmail.com>
2018-09-04 06:43:39 +0900

    add lesson 05

---
+++ 05_data/myapp.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+ 
+ // String. Char
+ val msg: String = "hello world"
+ val c: Char = 'a'
+ println(msg)
+ println(c)
+
+ // Byte, Short, Int , Long
+ val i: Int = 100
+ val l: Long = 55555555555555L
+ println(i)
+ println(l)
+ 
+ // Float, Double
+ val d: Double = 234.567
+ val f: Float = 12.3444F
+ println(d)
+ println(f)
+ 
+ // Boolean (true / false)
+ val flag: Boolean = true
+ println(flag)
+
+}
+
