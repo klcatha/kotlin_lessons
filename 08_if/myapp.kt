a7a1584c4864887ab740665b61a573c980145c7e
klcatha <klcatha0518@gmail.com>
2018-09-04 07:38:38 +0900

    add lesson 08

---
+++ 08_if/myapp.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+  // if
+  val score = 45
+  // > >= < <= == !=
+  /*if (score > 80) {
+    println("great!")
+  }else if (score > 60){
+    println("good!")
+  }else{
+    println("so so ...")
+  }*/
+  val result = if (score > 80) "Great" else "so so ..."
+  println(result)
+}
