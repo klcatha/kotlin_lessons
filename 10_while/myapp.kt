654f1c0e8088250c95be16a55aa23402ec1ed157
klcatha <klcatha0518@gmail.com>
2018-09-04 11:24:23 +0900

    add lesson 10

---
+++ 10_while/myapp.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+  // while
+  // 0 .. 9
+  
+  var i = 100
+  while(i < 10){
+    println("loop: $i")
+    i++
+  }
+
+  do {
+    println("loop2: $i")
+    i++
+  } while(i < 10)
+}
