2799c3d655f98f90a7634c75797e9875b4bc32a2
klcatha <klcatha0518@gmail.com>
2018-09-04 13:11:42 +0900

    add lesson 11

---
+++ 11_for/myapp.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+  // for(変数 in データの集合) 処理
+  
+  // break
+  // continue
+  for ( i in 0..9 ) {
+   /* if (i == 5) break*/
+   if (i == 5) continue
+    println(i)
+  }
+}
